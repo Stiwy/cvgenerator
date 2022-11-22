65f8271f55a7e9d96ee37a214d1522c72184752d
Stiwy Caritey <stiwy.caritey@gmail.com>
2022-11-22 17:31:27 +0100

    First commit

---
+++ tests/HomeTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests;
+
+use App\Controller\HomeController;
+use PHPUnit\Framework\TestCase;
+
+class HomeTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
